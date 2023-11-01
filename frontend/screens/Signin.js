29b527a3e4dce40b186d4ce456f582bbd504915e
Oscar Goes <goesoscar@utexas.edu>
2023-10-31 20:11:04 -0500

    Added stylesheets

--- frontend/screens/Signin.js
+++ frontend/screens/Signin.js
@@ -2,12 +2,31 @@ import { View, Text, StyleSheet, Button, Pressable } from "react-native";
 
 const Signin = () => {
   return (
-    <View className="Signin">
+    <View className="Signin" style={styles.container}>
       <Text>Welcome</Text>
       <Text>Login:</Text>
       <Text>Password:</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#aa",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "600",
+    color: "white",
+  },
+  body: {
+    fontSize: 24,
+    fontWeight: "200",
+    color: "white",
+  },
+});
 
 export default Signin;
