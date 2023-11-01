3c1e01af281320a61b32ccb2dd49220d6d8939dd
Oscar Goes <goesoscar@utexas.edu>
2023-10-31 20:11:04 -0500

    Added stylesheets

--- frontend/screens/Homepage.js
+++ frontend/screens/Homepage.js
@@ -3,10 +3,30 @@ import { View, Text, StyleSheet, Button, Pressable } from "react-native";
 
 const Homepage = () => {
   return (
-    <View className="Homepage">
+    <View className="Homepage" style={styles.container}>
       <Text>THIS IS THE HOME PAGE</Text>
+      <Text>There will be a map here WIP</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#aa",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "600",
+    color: "white",
+  },
+  body: {
+    fontSize: 24,
+    fontWeight: "200",
+    color: "white",
+  },
+});
 
 export default Homepage;
