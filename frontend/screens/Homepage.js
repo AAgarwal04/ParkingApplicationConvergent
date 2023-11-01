0643f5e892ba3ce34e42706a563618828e80d05b
Oscar Goes <goesoscar@utexas.edu>
2023-10-31 19:54:05 -0500

    Added Tab bar and fied some bugs

---
+++ frontend/screens/Homepage.js
@@ -0,0 +1,12 @@
+import { Navbar } from "../Navbar";
+import { View, Text, StyleSheet, Button, Pressable } from "react-native";
+
+const Homepage = () => {
+  return (
+    <View className="Homepage">
+      <Text>THIS IS THE HOME PAGE</Text>
+    </View>
+  );
+};
+
+export default Homepage;
---
+++ frontend/screens/Signin.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet, Button, Pressable } from "react-native";
+
+const Signin = () => {
+  return (
+    <View className="Signin">
+      <Text>Welcome</Text>
+      <Text>Login:</Text>
+      <Text>Password:</Text>
+    </View>
+  );
+};
+
+export default Signin;
