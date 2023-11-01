ada7ffdb6efe7c4fc47f9f1cddf323d582330a59
Oscar Goes <goesoscar@utexas.edu>
2023-10-31 20:20:24 -0500

    Fixed some bugs in settings

--- frontend/screens/Settings.js
+++ frontend/screens/Settings.js
@@ -26,11 +26,24 @@ const Settings = () => {
         </View>
     );
 }
- 
-
 
 const styles = StyleSheet.create({
-
-});
+    container: {
+      flex: 1,
+      backgroundColor: "#aa",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    title: {
+      fontSize: 40,
+      fontWeight: "600",
+      color: "white",
+    },
+    body: {
+      fontSize: 24,
+      fontWeight: "200",
+      color: "white",
+    },
+  });
 
 export default Settings;
